dd512ae7e6d16dedc87a278849fbd649c004f422
zivyll <zivyangll@gmail.com>
2017-11-29 20:02:58 +0800

    update proxy input

--- src/components/ProxyInputList.jsx
+++ src/components/ProxyInputList.jsx
@@ -12,9 +12,7 @@ import './ProxyInputList.less';
 
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
-
 let uuid = 0;
-let isServerData = false;
 
 class DynamicFieldSet extends Component {
 
@@ -25,31 +23,43 @@ class DynamicFieldSet extends Component {
       currentProxyIndex: 1, // 当前选中的代理地址
       proxies: [], // 当前备选代理地址
       originKeys: [],
+      isErrorInput: {},
     };
   }
 
-  componentWillReceiveProps = (props) => { // TODO input onchange will fire componentWillReceiveProps，hack by flag
-    if (props.proxyContent && !isServerData) {
-      isServerData = true;
+  componentWillReceiveProps = (props) => {
+    if (props.proxyContent) {
       const origin = JSON.parse(props.proxyContent);
       const lastKey = origin.originKeys[origin.originKeys.length - 1]
       if (lastKey) {
         uuid += lastKey
       }
       this.setState(origin);
+    } else {
+      this.setState({
+        useProxy: false,
+        currentProxyIndex: 1,
+        proxies: [],
+        originKeys: [],
+      })
     }
   }
 
   remove = (k) => {
     const newKeys = this.state.originKeys.filter(key => key !== k);
+    const index = this.state.originKeys.indexOf(k);
+    const newPorxies = [].concat(this.state.proxies)
+    newPorxies.splice(index, 1);
     if (k == this.state.currentProxyIndex) {
       this.setState({
         originKeys: newKeys,
+        proxies: newPorxies,
         currentProxyIndex: newKeys[0],
       });
     } else {
       this.setState({
         originKeys: newKeys,
+        proxies: newPorxies,
       });
     }
   }
@@ -57,13 +67,16 @@ class DynamicFieldSet extends Component {
   add = () => {
     uuid++;
     const newKeys = this.state.originKeys.concat(uuid);
+    const newProxies = this.state.proxies.concat('');
     if (this.state.originKeys.length) {
       this.setState({
         originKeys: newKeys,
+        proxies: newProxies,
       })
     } else {
       this.setState({
         originKeys: newKeys,
+        proxies: newProxies,
         currentProxyIndex: newKeys[0],
       })
     }
@@ -83,22 +96,35 @@ class DynamicFieldSet extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    var proxies = [];
-    this.props.form.validateFields((err, values) => {
-      this.state.originKeys.forEach(key => {
-        var proxy = values[`proxy-${key}`];
-        proxies.push(proxy)
-      });
-      if (!err) {
-        const result = {
-          proxies,
-          useProxy: this.state.useProxy,
-          originKeys: this.state.originKeys,
-          currentProxyIndex: this.state.currentProxyIndex,
-        }
-        this.props.onChangeProxy(JSON.stringify(result))
+    const urlReg = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i;
+    for(var i = 0; i < this.state.proxies.length; i++) {
+      const proxy = this.state.proxies[i];
+      this.state.isErrorInput[i] = 'ok';
+      const isErrorInput = JSON.parse(JSON.stringify(this.state.isErrorInput));
+      if (!urlReg.test(proxy)) {
+        isErrorInput[i] = 'error';
+        this.setState({
+          isErrorInput,
+        })
+        alert(`proxy url: ${proxy} is invalid`);
+        return;
       }
-    });
+    }
+    const result = {
+      proxies: this.state.proxies,
+      useProxy: this.state.useProxy,
+      originKeys: this.state.originKeys,
+      currentProxyIndex: this.state.currentProxyIndex,
+    }
+    this.props.onChangeProxy(JSON.stringify(result))
+  }
+
+  proxyInputChange = (e, index) => {
+    const originProxies = [].concat(this.state.proxies)
+    originProxies[index] = e.target.value
+    this.setState({
+      proxies: originProxies,
+    })
   }
 
   render() {
@@ -123,16 +149,16 @@ class DynamicFieldSet extends Component {
             required={true}
           >
           <div>
-            {getFieldDecorator(`proxy-${k}`, {
-              initialValue: this.state.proxies[index],
-              rules: [{
-                required: true,
-                pattern: /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i,
-                message: '请填写 URL 格式的代理地址',
-              }],
-            })(
-              <Input disabled={!this.state.useProxy} placeholder="请填写代理地址" style={{ width: '355px', marginRight: 8 }} />
-            )}
+            <Input
+              className={this.state.isErrorInput[index] === 'error' ? 'error-input' : ''}
+              value={this.state.proxies[index]}
+              onChange={(e) => {
+                this.proxyInputChange(e, index)
+              }}
+              disabled={!this.state.useProxy}
+              placeholder="请填写代理地址"
+              style={{ width: '355px', marginRight: 8 }}
+            />
             <Button
               size="small"
               className="dynamic-delete-button"
