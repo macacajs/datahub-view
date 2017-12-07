9e5416b74505a88949c12f4856542869313a9d2e
xdf <xudafeng@126.com>
2017-12-07 15:26:14 +0800

    Update helper.js

--- src/common/helper.js
+++ src/common/helper.js
@@ -4,7 +4,7 @@ import typeDetect from 'type-detect';
 const _ = lodash.merge({}, lodash);
 
 _.guid = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : r & 0x3 | 0x8;
     return v.toString(16);
