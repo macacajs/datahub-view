9b726422006ae1694861ef080a1026c55817b706
xudafeng <xudafeng@126.com>
2017-12-18 17:34:30 +0800

    tweak testcase

--- test/datahub-view.test.js
+++ test/datahub-view.test.js
@@ -6,31 +6,36 @@ import {
 } from './helper';
 
 describe('test/datahub-view.test.js', () => {
-  before(() => {
-    return driver
-      .initWindow({
-        width: 800,
-        height: 600,
-        deviceScaleFactor: 2
-      });
-  });
 
-  afterEach(function () {
-    return driver
-      .coverage()
-      .saveScreenshots(this);
-  });
+  describe('page func testing', () => {
 
-  after(() => {
-    return driver
-      .openReporter(false)
-      .quit();
-  });
+    before(() => {
+      return driver
+        .initWindow({
+          width: 800,
+          height: 600,
+          deviceScaleFactor: 2
+        });
+    });
 
-  it('page render should be ok', () => {
-    return driver
-      .get(BASE_URL)
-      .sleep(5000);
+    afterEach(function () {
+      return driver
+        .coverage()
+        .saveScreenshots(this);
+    });
+
+    after(() => {
+      return driver
+        .openReporter(false)
+        .quit();
+    });
+
+    it('page render should be ok', () => {
+      return driver
+        .get(BASE_URL)
+        .sleep(5000);
+    });
+
   });
 
 });
