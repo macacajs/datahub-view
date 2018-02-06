e63da929f6ca837387a0df2ba3101ecb1948447f
xudafeng <xudafeng@126.com>
2018-02-06 17:28:42 +0800

    remove bin

--- bin/datahub-view.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const { join } = require('path');
-const { spawn } = require('child_process');
-
-const cwd = join(require.resolve('..'), '..', '..');
-spawn('npm', ['run', 'dev'], { cwd });
-
