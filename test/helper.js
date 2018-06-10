8db6f298ca7f89482d0c2a5d0ccd3e5b5446b88e
zivyll <zivyangll@gmail.com>
2018-06-10 13:52:39 +0800

    test: set value of CodeMirror

--- test/helper.js
+++ test/helper.js
@@ -20,6 +20,7 @@ exports.driver = driver;
 exports.BASE_URL = BASE_URL;
 
 exports.setCodeMirror = d => {
-  const str = JSON.stringify(d, null, 2);
-  return `document.querySelector('.CodeMirror').CodeMirror.setValue(${str})`;
+  /* eslint-disable */
+  return `document.querySelector('.CodeMirror').CodeMirror.setValue(\'${JSON.stringify(d)}\')`
+  /* eslint-enable */
 };
