76b923e3f8c889403e4c5121c7324065c37acc34
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-10-23 11:11:33 +0800

    fix: adjust locale message

--- src/locale/en_US.js
+++ src/locale/en_US.js
@@ -77,8 +77,8 @@ const enUS = {
 
   'proxyConfig.enable.true': 'Proxy mode enabled',
   'proxyConfig.enable.false': 'Proxy mode disabled',
-  'proxyConfig.globalEnable.true': 'Global proxy mode enabled',
-  'proxyConfig.globalEnable.false': 'Global proxy mode disabled',
+  'proxyConfig.globalEnable.true': 'All proxies Enabled',
+  'proxyConfig.globalEnable.false': 'All proxies Disabled',
   'proxyConfig.addProxyUrl': 'Add proxy url',
   'proxyConfig.invalidProxyUrl': 'Please input valid url',
   'proxyConfig.switchProxyUrlHint': 'Click to switch proxy url:',
