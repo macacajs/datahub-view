769cef3b99b3da3dbab08e483d514f702adc324d
carlisliu.lym <carlisliu.lym@alibaba-inc.com>
2020-03-26 14:42:46 +0800

    fixed: remove unused package

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
