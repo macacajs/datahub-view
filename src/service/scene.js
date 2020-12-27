d8afbff2679514a38bdb45e1d3606d5c28d384e4
xudafeng <xudafeng@126.com>
2020-12-27 14:52:53 +0800

    feat: support scene format

--- src/service/scene.js
+++ src/service/scene.js
@@ -4,21 +4,23 @@ export async function getSceneList ({ interfaceUniqId }) {
   return request(`/api/scene?interfaceUniqId=${interfaceUniqId}`, 'GET');
 };
 
-export async function createScene ({ interfaceUniqId, sceneName, contextConfig, data }) {
+export async function createScene ({ interfaceUniqId, sceneName, contextConfig, data, format }) {
   return request('/api/scene', 'POST', {
     interfaceUniqId,
     sceneName,
     contextConfig,
     data,
+    format,
   });
 };
 
-export async function updateScene ({ uniqId, interfaceUniqId, sceneName, contextConfig, data }) {
+export async function updateScene ({ uniqId, interfaceUniqId, sceneName, contextConfig, data, format }) {
   return request(`/api/scene/${uniqId}`, 'PUT', {
     interfaceUniqId,
     sceneName,
     contextConfig,
     data,
+    format,
   });
 };
 
